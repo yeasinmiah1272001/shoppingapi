8585ecb3047e515c2e29e14a24cb68446d9ffa9f
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-24 05:10:09 +0600

    spicy data add

--- index.js
+++ index.js
@@ -27,13 +27,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-      const bookCollection = client.db("shoppingdata").collection("shoppingdata");
-      const productCollection = client.db("shoppingdata").collection("products");
-    
-
-      
-    
-
+    const bookCollection = client.db("shoppingdata").collection("shoppingdata");
+    const productCollection = client.db("shoppingdata").collection("products");
+    const spicyCollection = client.db("shoppingdata").collection("spicy");
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -43,33 +39,29 @@ async function run() {
 
     // auth releted appi
 
-   app.get("/shopping", async(req, res) =>{
-    const result = await bookCollection.find().toArray();
-    res.send(result)
-   })
-   app.get("/products", async(req, res) =>{
-    const result = await productCollection.find().toArray();
-    res.send(result)
-   })
+    app.get("/shopping", async (req, res) => {
+      const result = await bookCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/allspicy", async (req, res) => {
+      const result = await spicyCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/", (req, res) => {
+      res.send("api-running...");
+    });
 
- app.get("/", (req, res) => {
-   res.send("api-running...");
- });
-
- 
-
-
-
- app.listen(port, () => {
-   console.log(`Simple Crud is Running on port ${port}`);
- });
-
-
-
+    app.listen(port, () => {
+      console.log(`Simple Crud is Running on port ${port}`);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
